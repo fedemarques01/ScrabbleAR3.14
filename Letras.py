57c35a94fceaead3e88be3c6c5883945849f0f84
agcastro1994 <agcastro1994@gmail.com>
2020-06-04 16:20:50 -0300

    ficha ya no es un objeto, bolsa y atril son listas de strings

--- Letras.py
+++ Letras.py
@@ -1,6 +1,6 @@
 import random
 
-valoresLetras = {"A": 1,
+valoresLetras= {"A": 1,
                  "B": 3,
                  "C": 3,
                  "CH":5,
@@ -28,27 +28,13 @@ valoresLetras = {"A": 1,
                  "X": 8,
                  "Y": 4,
                  "Z": 10,
-                 "#": 0}
+                 "#": 0}  
 
-class Ficha:
-    """
-    Clase que crea una ficha. Asigna a una ficha una letra y la referencia a su valor en el diccionario valoresLetras
-    """
-    def __init__(self, letra, valoresLetras):
-        
-        self.letra = letra.upper()
-        if self.letra in valoresLetras:
-            self.valor = valoresLetras[self.letra]
-        else:
-            self.valor = 0
 
-    def get_letra(self):
-        
-        return self.letra
 
-    def get_valor(self):
+def get_valor(letra,valoresLetras):
         
-        return self.valor
+    return valoresLetras[letra]
 
 class Bolsa:
 
@@ -58,116 +44,116 @@ class Bolsa:
          self.dificultad = ['facil', 'media', 'dificil']
          self.initialize_bolsa(nivel)
 
-     def agregar_bolsa(self, ficha, cantidad):
-        
+     def agregar_bolsa(self, letra, cantidad):
+         mayus = letra.upper()
          for i in range(cantidad):
-             self.bolsa.append(ficha)
+             self.bolsa.append(mayus)
 
      def initialize_bolsa(self,nivel):
-         global valoresLetras
+               
          if nivel == self.dificultad[0]:        
-             #bolsa de fichas nivel facil, distribucion de fichas clasica del scrabble en espa;ol
-             self.agregar_bolsa(Ficha("A", valoresLetras), 12)
-             self.agregar_bolsa(Ficha("B", valoresLetras), 2)
-             self.agregar_bolsa(Ficha("C", valoresLetras), 4)
-             #self.agregar_bolsa(Ficha("CH", valoresLetras), 1)
-             self.agregar_bolsa(Ficha("D", valoresLetras), 5)
-             self.agregar_bolsa(Ficha("E", valoresLetras), 12)
-             self.agregar_bolsa(Ficha("F", valoresLetras), 1)
-             self.agregar_bolsa(Ficha("G", valoresLetras), 2)
-             self.agregar_bolsa(Ficha("H", valoresLetras), 2)
-             self.agregar_bolsa(Ficha("I", valoresLetras), 6)
-             self.agregar_bolsa(Ficha("J", valoresLetras), 1)       
-             self.agregar_bolsa(Ficha("L", valoresLetras), 4)
-             #self.agregar_bolsa(Ficha("LL", valoresLetras), 1)
-             self.agregar_bolsa(Ficha("M", valoresLetras), 2)
-             self.agregar_bolsa(Ficha("N", valoresLetras), 5)
-             self.agregar_bolsa(Ficha("Ñ", valoresLetras), 1)
-             self.agregar_bolsa(Ficha("O", valoresLetras), 9)
-             self.agregar_bolsa(Ficha("P", valoresLetras), 2)
-             self.agregar_bolsa(Ficha("Q", valoresLetras), 1)
-             self.agregar_bolsa(Ficha("R", valoresLetras), 5)
-            # self.agregar_bolsa(Ficha("RR", valoresLetras), 1)
-             self.agregar_bolsa(Ficha("S", valoresLetras), 6)
-             self.agregar_bolsa(Ficha("T", valoresLetras), 4)
-             self.agregar_bolsa(Ficha("U", valoresLetras), 5)
-             self.agregar_bolsa(Ficha("V", valoresLetras), 1)
-             self.agregar_bolsa(Ficha("X", valoresLetras), 1)
-             self.agregar_bolsa(Ficha("Y", valoresLetras), 1)
-             self.agregar_bolsa(Ficha("Z", valoresLetras), 1)
-            # self.agregar_bolsa(Ficha("#", valoresLetras), 2)
+             #bolsa nivel facil, distribucion  clasica del scrabble en espa;ol
+             self.agregar_bolsa("A", 12)
+             self.agregar_bolsa("B", 2)
+             self.agregar_bolsa("C", 4)
+             #self.agregar_bolsa("CH", 1)
+             self.agregar_bolsa("D", 5)
+             self.agregar_bolsa("E", 12)
+             self.agregar_bolsa("F", 1)
+             self.agregar_bolsa("G", 2)
+             self.agregar_bolsa("H", 2)
+             self.agregar_bolsa("I", 6)
+             self.agregar_bolsa("J", 1)       
+             self.agregar_bolsa("L", 4)
+             #self.agregar_bolsa("LL", 1)
+             self.agregar_bolsa("M", 2)
+             self.agregar_bolsa("N", 5)
+             self.agregar_bolsa("Ñ", 1)
+             self.agregar_bolsa("O", 9)
+             self.agregar_bolsa("P", 2)
+             self.agregar_bolsa("Q", 1)
+             self.agregar_bolsa("R", 5)
+            # self.agregar_bolsa("RR", 1)
+             self.agregar_bolsa("S", 6)
+             self.agregar_bolsa("T", 4)
+             self.agregar_bolsa("U", 5)
+             self.agregar_bolsa("V", 1)
+             self.agregar_bolsa("X", 1)
+             self.agregar_bolsa("Y", 1)
+             self.agregar_bolsa("Z", 1)
+            # self.agregar_bolsa("#", 2)
 
          if nivel == self.dificultad[1]:        
-             #bolsa de fichas nivel media, se disminuyen algunas vocales y se reducen las consonantes de 1 y 2 puntos
-             self.agregar_bolsa(Ficha("A", valoresLetras), 10)
-             self.agregar_bolsa(Ficha("B", valoresLetras), 2)
-             self.agregar_bolsa(Ficha("C", valoresLetras), 4)
-             #self.agregar_bolsa(Ficha("CH", valoresLetras), 1)
-             self.agregar_bolsa(Ficha("D", valoresLetras), 3)
-             self.agregar_bolsa(Ficha("E", valoresLetras), 10)
-             self.agregar_bolsa(Ficha("F", valoresLetras), 1)
-             self.agregar_bolsa(Ficha("G", valoresLetras), 2)
-             self.agregar_bolsa(Ficha("H", valoresLetras), 2)
-             self.agregar_bolsa(Ficha("I", valoresLetras), 6)
-             self.agregar_bolsa(Ficha("J", valoresLetras), 1)       
-             self.agregar_bolsa(Ficha("L", valoresLetras), 2)
-             #self.agregar_bolsa(Ficha("LL", valoresLetras), 1)
-             self.agregar_bolsa(Ficha("M", valoresLetras), 2)
-             self.agregar_bolsa(Ficha("N", valoresLetras), 4)
-             self.agregar_bolsa(Ficha("Ñ", valoresLetras), 1)
-             self.agregar_bolsa(Ficha("O", valoresLetras), 8)
-             self.agregar_bolsa(Ficha("P", valoresLetras), 2)
-             self.agregar_bolsa(Ficha("Q", valoresLetras), 1)
-             self.agregar_bolsa(Ficha("R", valoresLetras), 4)
-             #self.agregar_bolsa(Ficha("RR", valoresLetras), 1)
-             self.agregar_bolsa(Ficha("S", valoresLetras), 4)
-             self.agregar_bolsa(Ficha("T", valoresLetras), 2)
-             self.agregar_bolsa(Ficha("U", valoresLetras), 4)
-             self.agregar_bolsa(Ficha("V", valoresLetras), 1)
-             self.agregar_bolsa(Ficha("X", valoresLetras), 1)
-             self.agregar_bolsa(Ficha("Y", valoresLetras), 1)
-             self.agregar_bolsa(Ficha("Z", valoresLetras), 1)
-             #self.agregar_bolsa(Ficha("#", valoresLetras), 2)
+             #bolsa nivel media, se disminuyen algunas vocales y se reducen las consonantes de 1 y 2 puntos
+             self.agregar_bolsa("A", 10)
+             self.agregar_bolsa("B", 2)
+             self.agregar_bolsa("C", 4)
+             #self.agregar_bolsa("CH", 1)
+             self.agregar_bolsa("D", 3)
+             self.agregar_bolsa("E", 10)
+             self.agregar_bolsa("F", 1)
+             self.agregar_bolsa("G", 2)
+             self.agregar_bolsa("H", 2)
+             self.agregar_bolsa("I", 6)
+             self.agregar_bolsa("J", 1)       
+             self.agregar_bolsa("L", 2)
+             #self.agregar_bolsa("LL", 1)
+             self.agregar_bolsa("M", 2)
+             self.agregar_bolsa("N", 4)
+             self.agregar_bolsa("Ñ", 1)
+             self.agregar_bolsa("O", 8)
+             self.agregar_bolsa("P", 2)
+             self.agregar_bolsa("Q", 1)
+             self.agregar_bolsa("R", 4)
+             #self.agregar_bolsa("RR", 1)
+             self.agregar_bolsa("S", 4)
+             self.agregar_bolsa("T", 2)
+             self.agregar_bolsa("U", 4)
+             self.agregar_bolsa("V", 1)
+             self.agregar_bolsa("X", 1)
+             self.agregar_bolsa("Y", 1)
+             self.agregar_bolsa("Z", 1)
+             #self.agregar_bolsa("#", 2)
 
          if nivel == self.dificultad[2]:        
-             #bolsa de fichas nivel dificil, se disminuyen aun mas las vocales
-             self.agregar_bolsa(Ficha("A", valoresLetras), 8)
-             self.agregar_bolsa(Ficha("B", valoresLetras), 2)
-             self.agregar_bolsa(Ficha("C", valoresLetras), 4)
-             #self.agregar_bolsa(Ficha("CH", valoresLetras), 1)
-             self.agregar_bolsa(Ficha("D", valoresLetras), 3)
-             self.agregar_bolsa(Ficha("E", valoresLetras), 7)
-             self.agregar_bolsa(Ficha("F", valoresLetras), 1)
-             self.agregar_bolsa(Ficha("G", valoresLetras), 2)
-             self.agregar_bolsa(Ficha("H", valoresLetras), 2)
-             self.agregar_bolsa(Ficha("I", valoresLetras), 4)
-             self.agregar_bolsa(Ficha("J", valoresLetras), 1)       
-             self.agregar_bolsa(Ficha("L", valoresLetras), 2)
-             #self.agregar_bolsa(Ficha("LL", valoresLetras), 1)
-             self.agregar_bolsa(Ficha("M", valoresLetras), 2)
-             self.agregar_bolsa(Ficha("N", valoresLetras), 4)
-             self.agregar_bolsa(Ficha("Ñ", valoresLetras), 1)
-             self.agregar_bolsa(Ficha("O", valoresLetras), 6)
-             self.agregar_bolsa(Ficha("P", valoresLetras), 2)
-             self.agregar_bolsa(Ficha("Q", valoresLetras), 1)
-             self.agregar_bolsa(Ficha("R", valoresLetras), 4)
-             #self.agregar_bolsa(Ficha("RR", valoresLetras), 1)
-             self.agregar_bolsa(Ficha("S", valoresLetras), 4)
-             self.agregar_bolsa(Ficha("T", valoresLetras), 2)
-             self.agregar_bolsa(Ficha("U", valoresLetras), 3)
-             self.agregar_bolsa(Ficha("V", valoresLetras), 1)
-             self.agregar_bolsa(Ficha("X", valoresLetras), 1)
-             self.agregar_bolsa(Ficha("Y", valoresLetras), 1)
-             self.agregar_bolsa(Ficha("Z", valoresLetras), 1)
-             #self.agregar_bolsa(Ficha("#", valoresLetras), 2)
+             #bolsa nivel dificil, se disminuyen aun mas las vocales
+             self.agregar_bolsa("A", 8)
+             self.agregar_bolsa("B", 2)
+             self.agregar_bolsa("C", 4)
+             #self.agregar_bolsa("CH", 1)
+             self.agregar_bolsa("D", 3)
+             self.agregar_bolsa("E", 7)
+             self.agregar_bolsa("F", 1)
+             self.agregar_bolsa("G", 2)
+             self.agregar_bolsa("H", 2)
+             self.agregar_bolsa("I", 4)
+             self.agregar_bolsa("J", 1)       
+             self.agregar_bolsa("L", 2)
+             #self.agregar_bolsa("LL", 1)
+             self.agregar_bolsa("M", 2)
+             self.agregar_bolsa("N", 4)
+             self.agregar_bolsa("Ñ", 1)
+             self.agregar_bolsa("O", 6)
+             self.agregar_bolsa("P", 2)
+             self.agregar_bolsa("Q", 1)
+             self.agregar_bolsa("R", 4)
+             #self.agregar_bolsa("RR", 1)
+             self.agregar_bolsa("S", 4)
+             self.agregar_bolsa("T", 2)
+             self.agregar_bolsa("U", 3)
+             self.agregar_bolsa("V", 1)
+             self.agregar_bolsa("X", 1)
+             self.agregar_bolsa("Y", 1)
+             self.agregar_bolsa("Z", 1)
+             #self.agregar_bolsa("#", 2)
          random.shuffle(self.bolsa)
 
      def tomar_bolsa(self):
-         #Toma la primera ficha de la bolsa y la elimina de la misma. Se usa para cambiar fichas o la recarga del atril (con un ciclo)
+         #Toma la primera de la bolsa y la elimina de la misma. Se usa para cambiars o la recarga del atril (con un ciclo)
          return self.bolsa.pop()
 
-     def cantidad_fichas(self):
-         #Retorna la cantidad de fichas restantes
+     def cantidad_Fichas(self):
+         #Retorna la cantidad des restantes
          return len(self.bolsa)
 
 class Atril:
@@ -180,66 +166,72 @@ class Atril:
         self.bolsa = bolsa
         self.initialize()
 
+
+
+    def initialize(self):
+        
+        for i in range(7):
+            self.agregar_al_atril()
+
+
     def agregar_al_atril(self):
        
         self.atril.append(self.bolsa.tomar_bolsa())
 
-    def initialize(self):
-        
-         for i in range(7):
-             self.agregar_al_atril()
-
-
-
 
     def get_atril_string(self):
         #Devuelve el atril en forma de string
-        return ", ".join(str(item.get_letra()) for item in self.atril)
+        return ", ".join(str(item) for item in self.atril)
 
     def get_atril_array(self):
         #Devuelve el atril
         return self.atril
 
-    def usar_ficha(self, ficha):
-        #Quita la ficha del atril, para ser cambiada o jugada
-        self.atril.remove(ficha)
+    def usar(self,letra):
+        #Quita la del atril, para ser cambiada o jugada
+        self.atril.remove(letra)
 
     def get_atril_espaciosVacios(self):
-        #Cantidad de fichas que faltan en el atril
+        #Cantidad des que faltan en el atril
         return 7 - len(self.atril)
 
     def get_atril_tamanio(self):
-        #Cantidad de fichas en el atril
+        #Cantidad des en el atril
         return len(self.atril)
 
     def rellenar_atril(self):
         #
-        while self.get_atril_espaciosVacios() > 0 and self.bolsa.cantidad_fichas() > 0:
+        while self.get_atril_espaciosVacios() > 0 and self.bolsa.cantidad_Fichas() > 0:
             self.agregar_al_atril()
 
-#Codigo de prueba, genera una bolsa segun la dificultad dada en el constructor, imprime las fichas disponibles y cantidad total de fichas 
+#Codigo de prueba, genera una bolsa segun la dificultad dada en el constructor, imprime lass disponibles y cantidad total des 
 # de la bolsa. 
 
 
 bolsa1 = Bolsa('facil')
-fichasTotales = bolsa1.cantidad_fichas()
+fichasTotales = bolsa1.cantidad_Fichas()
 
-for i in range(fichasTotales):
-    print(bolsa1.bolsa[i].letra)
-print('Total de fichas del nivel: facil ')
-print(bolsa1.cantidad_fichas())
+for i in range (fichasTotales):
+    print(bolsa1.bolsa[i])
+print('Total fichas del nivel: facil ')
+print(bolsa1.cantidad_Fichas())
 
 
 #Crea el atril del jugador y lo muestra en pantalla
 atril1 = Atril(bolsa1)
+atril2 = Atril(bolsa1)
 
 atril_jugador =  atril1.get_atril_string()
 print('Atril del jugador')
 print(atril_jugador)
 
-#fichas en la bolsa luego de crear el primer atril
-print('Fichas restantes')
-print(bolsa1.cantidad_fichas())
+atril_cpu =  atril2.get_atril_string()
+print('Atril del cpu')
+print(atril_cpu)
+
+#fichas en la bolsa luego de crear los 2 atriles
+print('cantidad Fichas restantes')
+print(bolsa1.cantidad_Fichas())
 
 #Se simula una jugada, pidiendo que se escriba una palabra
 # (no hay verificacion alguna, se asume que se usaran letras del atril y que toda palabra es valida)
@@ -255,9 +247,9 @@ for letra in palabra:
     i = 0
     arreglo = atril1.get_atril_array()
     while (encontre == False):
-        if letra == arreglo[i].letra:
+        if letra == arreglo[i]:
             encontre = True
-            atril1.usar_ficha(arreglo[i])
+            atril1.usar(arreglo[i])
         else:
             i=i+1
 
@@ -267,16 +259,16 @@ print('Atril del jugador luego de jugar')
 print(atril_jugador)
 
 
-#Se recargan fichas y se muestra el atril nuevo y 
-# la cantidad de fichas en la bolsa luego de todo el proceso
+#Se recargans y se muestra el atril nuevo y 
+# la cantidad des en la bolsa luego de todo el proceso
 atril1.rellenar_atril()
 
 atril_jugador =  atril1.get_atril_string()
 print('Atril del jugador luego de recargar')
 print(atril_jugador)
 
-print('Fichas restantes luego de recargar')
-print(bolsa1.cantidad_fichas())
+print('fichas restantes luego de recargar')
+print(bolsa1.cantidad_Fichas())
 
 
 
