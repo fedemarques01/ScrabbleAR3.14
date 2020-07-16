35673575004e8a15765d89272254f544a3021851
Basa <qksergbau@gmail.com>
2020-07-15 21:33:23 -0300

    errores que no saltaron nunca pero we

--- Ventanas/Puntuaciones.py
+++ Ventanas/Puntuaciones.py
@@ -16,7 +16,7 @@ def CargarDatos():
 def merge(dic):
     me, mm, mh = 0, 0, 0
     l = []
-    m = [dic['Easy'][0]+['Easy'], dic['Medium'][0]+['Medi'], dic['Hard'][0]+['Hard']]
+    m = [dic['easy'][0]+['Easy'], dic['medium'][0]+['Medi'], dic['hard'][0]+['Hard']]
     for i in range(10):
         maximo = max(m)
         #print('m',m)
@@ -28,23 +28,23 @@ def merge(dic):
             continue
 
         try:
-            if(maximo[0] == dic['Hard'][mh][0]):
+            if(maximo[0] == dic['hard'][mh][0]):
                 mh += 1
-                m.append(dic['Hard'][mh]+['Hard'])
+                m.append(dic['hard'][mh]+['Hard'])
         except IndexError:  # excepcion out of range => append 0
             m.append([0])
 
         try:
-            if(maximo[0] == dic['Medium'][mm][0]):
+            if(maximo[0] == dic['medium'][mm][0]):
                 mm += 1
-                m.append(dic['Medium'][mm]+['Medi'])
+                m.append(dic['medium'][mm]+['Medi'])
         except IndexError:  # excepcion out of range => append 0
             m.append([0])
 
         try:
-            if(maximo[0] == dic['Easy'][me][0]):
+            if(maximo[0] == dic['easy'][me][0]):
                 me += 1
-                m.append(dic['Easy'][me]+['Easy'])
+                m.append(dic['easy'][me]+['Easy'])
         except IndexError:  # excepcion out of range => append 0
             m.append([0])
 
@@ -57,6 +57,7 @@ def merge(dic):
 def CrearLista(filtro,dic):
     listaStr = []
     for i in range(1, 11):
+        algo=''
         try:
             algo = str(str(i)+': '+dic[filtro][i-1][1]+' -  '+str(dic[filtro][i-1][0]))
         except IndexError:
@@ -93,11 +94,11 @@ def listaPuntuacionesAltas():
         if event == "General":
             window.Element("lista").update(values=merge(dic))
         if event == "Facil":
-            window.Element("lista").update(values=CrearLista("Easy",dic))
+            window.Element("lista").update(values=CrearLista("easy",dic))
         if event == "Normal":
-            window.Element("lista").update(values=CrearLista("Medium",dic))
+            window.Element("lista").update(values=CrearLista("medium",dic))
         if event == "Dificil":
-            window.Element("lista").update(values=CrearLista("Hard",dic))
+            window.Element("lista").update(values=CrearLista("hard",dic))
     window.close()
 
 
